bd2a4817cd7fcc8fcfe8a96453e1030475f2137b
adrmzz <adrmzz@users.noreply.github.com>
2020-04-15 23:26:10 +0200

    Create sardinianplugin.h
    Copied from Catalan; just replaced __Catalan__ with __Sardinian__

---
+++ plugins/sc/src/sardinianplugin.h
@@ -0,0 +1,25 @@
+﻿#ifndef SARDINIANPLUGIN_H
+#define SARDINIANPLUGIN_H
+
+#include <QObject>
+#include "languageplugininterface.h"
+#include "westernlanguagesplugin.h"
+
+class CatalanPlugin : public WesternLanguagesPlugin
+{
+    Q_OBJECT
+    Q_INTERFACES(LanguagePluginInterface)
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.Examples.SardinianPlugin" FILE "sardinianplugin.json")
+
+public:
+    explicit SardinianPlugin(QObject* parent = 0)
+        : WesternLanguagesPlugin(parent)
+    {
+    }
+
+    virtual ~SardinianPlugin()
+    {
+    }
+};
+
+#endif // SARDINIANPLUGIN_H
