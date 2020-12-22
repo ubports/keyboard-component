50ab66976fb5b80f676f961aa647cde1b1a55c3b
Fuseteam <fusekai@outlook.com>
2020-12-22 09:40:39 -0300

    remove emoji as a language layout

--- qml/keys/languages.js
+++ qml/keys/languages.js
@@ -25,7 +25,6 @@ function languageIdToName(languageId)
     if (languageId == "cs")         return i18n.tr("Czech");
     if (languageId == "da")         return i18n.tr("Danish");
     if (languageId == "de")         return i18n.tr("German");
-    if (languageId == "emoji")      return i18n.tr("Emoji");
     if (languageId == "el")         return i18n.tr("Greek");
     if (languageId == "en")         return i18n.tr("English");
     if (languageId == "en@dv")      return i18n.tr("English\n(Dvorak)");
