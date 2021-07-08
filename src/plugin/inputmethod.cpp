d1e78c7acba0b28e8eef11de4b48fb819d181640
Abdullah AL Shohag <HackerShohag@outlook.com>
2021-07-08 11:10:31 +0600

    test

--- src/plugin/inputmethod.cpp
+++ src/plugin/inputmethod.cpp
@@ -683,6 +683,13 @@ const QString InputMethod::theme() const
     return d->m_settings.theme();
 }
 
+const QString InputMethod::avrotmp() const
+{
+    Q_D(InputMethod);
+    d->avrotmp = d->editor.text()->avrotmp();
+    return d->avrotmp;
+}
+
 void InputMethod::replacePreedit(const QString &preedit)
 {
     Q_D(InputMethod);
